3bd2bd59452567698dcb5ec158826ecccea90e82
Lai Xuancheng <lai32690@gmail.com>
2018-06-07 20:33:19 -0300

    passing functions to share and plus

--- src/Results/Deputado.js
+++ src/Results/Deputado.js
@@ -1,26 +1,29 @@
 import React from 'react';
 
-const Deputado = ({ deputadoName, deputadoNumber, matchPercent, partidoName }) => <div>
+const noop = () => {};
+
+const Deputado = ({ nome, numero, partido, afinidade,
+  onClickShare = noop, onClickPlus = noop }) => <div>
   <div className='photo'>
     <img src="http://via.placeholder.com/350x150" alt="" style={{ width: 60, height: 60 }} />
   </div>
   <div className='info'>
     <div>
-      <span className='candidat-name'>{deputadoName}</span>
-      <span className='candidat-number'>{deputadoNumber}</span>
+      <span className='candidat-name'>{nome}</span>
+      <span className='candidat-number'>{numero}</span>
     </div>
 
     <div>
-      <span className='partido-name'>{partidoName}</span>
+      <span className='partido-name'>{partido}</span>
     </div>
 
     <div>
-      Afinidade: <span className='match-percent'>{matchPercent}%</span>
+      Afinidade: <span className='match-percent'>{afinidade}%</span>
     </div>
   </div>
   <div className='buttons'>
-    <button>+</button>
-    <button>+</button>
+    <button onClick={onClickShare}>+</button>
+    <button onClick={onClickPlus}>+</button>
   </div>
 </div>
 
