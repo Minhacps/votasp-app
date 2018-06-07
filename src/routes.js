f40bc77d994f0374833d21f1eb932144f8458400
gabriel <gabriel@MacBook-Pro-de-MacBook.local>
2018-06-07 00:08:55 -0300

    Adiciona Header ao arquivo principal de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
 
+import Header from './components/Header/Header';
 import Auth0 from './Auth0/Auth0';
 import Auth0Callback from './Auth0/Auth0.callback';
 import renderAuthenticated from './Auth0/renderAuthenticated';
@@ -8,15 +9,19 @@ import renderAuthenticated from './Auth0/renderAuthenticated';
 import Home from './Home/Home';
 import history from './history';
 
+
 export const makeMainRoutes = () => {
   const auth = new Auth0();
 
   return (
-    <Router history={history}>
-      <Switch>
-        <Route exact path='/' render={(props) => renderAuthenticated(Home, props)} />
-        <Route exact path='/callback' render={(props) => <Auth0Callback auth={auth} {...props} />} />
-      </Switch>
+    <React.Fragment>
+      <Header />
+      <Router history={history}>
+        <Switch>
+          <Route exact path='/' render={(props) => renderAuthenticated(Home, props)} />
+          <Route exact path='/callback' render={(props) => <Auth0Callback auth={auth} {...props} />} />
+        </Switch>
     </Router>
+    </React.Fragment>
   );
 };
