6a645824b65127ea7b85fbfb8d281402e86bbc38
Victor Perin <me@victorperin.ninja>
2018-06-10 19:35:06 -0300

    feat(components/WelcomeMessage): dynamic regionTag property for multiple regions use

--- src/components/WelcomeMessage/WelcomeMessage.js
+++ src/components/WelcomeMessage/WelcomeMessage.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './WelcomeMessage.scss';
 
-const WelcomeMessage = () => {
+const regions = {
+  sp: { name: 'São Paulo', projectName: 'VotaSP' },
+};
+
+const WelcomeMessage = ({ regionTag = 'sp' }) => {
+  const region = regions[regionTag];
+
   return (
     <section>
 			<div className="container welcome-text">
 				<h2><strong>BEM-VINDO</strong></h2>
-				<p>O projeto VotaSP nasceu com o objetivo de ajudar a população do estado de São Paulo a escolher seus candidatos. Através de questões objetivas os eleitores podem verificar suas afinidades com os candidatos.<br /><strong>É muito simples e prático!</strong></p>
+				<p>O projeto {region.projectName} nasceu com o objetivo de ajudar a população do estado de {region.name} a escolher seus candidatos. Através de questões objetivas os eleitores podem verificar suas afinidades com os candidatos.<br /><strong>É muito simples e prático!</strong></p>
 
         <Link to={`/perguntas`} className="btn btn-primary">
           Começar
