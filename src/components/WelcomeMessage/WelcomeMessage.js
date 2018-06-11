db32774d5b212c54b3d5503068a62d97f932d10d
Victor Miguez <victorcineis@gmail.com>
2018-06-11 00:45:49 -0300

    Altera extensão .scss para .css do WelcomeMessage

--- src/components/WelcomeMessage/WelcomeMessage.js
+++ src/components/WelcomeMessage/WelcomeMessage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './WelcomeMessage.scss';
+import './WelcomeMessage.css';
 
 const regions = {
-  sp: { name: 'São Paulo', projectName: 'VotaSP' },
+  sp: { name: 'São Paulo', projectName: 'VotaSP' }
 };
 
 const WelcomeMessage = ({ regionTag }) => {
@@ -11,15 +11,23 @@ const WelcomeMessage = ({ regionTag }) => {
 
   return (
     <section>
-			<div className="container welcome-text">
-				<h2><strong>BEM-VINDO</strong></h2>
-				<p>O projeto {region.projectName} nasceu com o objetivo de ajudar a população do estado de {region.name} a escolher seus candidatos. Através de questões objetivas os eleitores podem verificar suas afinidades com os candidatos.<br /><strong>É muito simples e prático!</strong></p>
+      <div className="container welcome-text">
+        <h2>
+          <strong>BEM-VINDO</strong>
+        </h2>
+        <p>
+          O projeto {region.projectName} nasceu com o objetivo de ajudar a
+          população do estado de {region.name} a escolher seus candidatos.
+          Através de questões objetivas os eleitores podem verificar suas
+          afinidades com os candidatos.<br />
+          <strong>É muito simples e prático!</strong>
+        </p>
 
         <Link to={`/perguntas`} className="btn btn-primary">
           Começar
         </Link>
-			</div>
-		</section>
+      </div>
+    </section>
   );
 };
 
