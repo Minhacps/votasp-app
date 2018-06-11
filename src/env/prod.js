e466c50fab521cb1ec8571d7c7aed8bc9e6e5f56
Victor Miguez <victorcineis@gmail.com>
2018-06-11 20:39:02 -0300

    Remove custom domain do auth0

--- src/env/prod.js
+++ src/env/prod.js
@@ -1,7 +1,7 @@
 export default {
   auth0: {
     originalDomain: 'minhacampinas.auth0.com',
-    domain: 'login.votasp.org.br',
+    domain: 'minhacampinas.auth0.com',
     clientId: 'vYyUQhCzwFmmw0iZcxTXZM8A6Ufw6PtL',
     callbackUrl: 'https://app.votasp.org.br/login/callback',
     responseType: 'token id_token',
