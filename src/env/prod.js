ed189e8a0561814f1fca852326a74a23b2beda2b
Victor Miguez <victorcineis@gmail.com>
2018-06-11 19:36:57 -0300

    Atualiza ID de prod do auth0

--- src/env/prod.js
+++ src/env/prod.js
@@ -2,7 +2,7 @@ export default {
   auth0: {
     originalDomain: 'minhacampinas.auth0.com',
     domain: 'login.votasp.org.br',
-    clientId: '0511QuKo8LMyLODIwp2mcKbFskxruEsx',
+    clientId: 'vYyUQhCzwFmmw0iZcxTXZM8A6Ufw6PtL',
     callbackUrl: 'https://app.votasp.org.br/login/callback',
     responseType: 'token id_token',
     scope: 'openid'
