64ccde2a485701ea73d02fdedd841ed6bc855793
Victor Miguez <victorcineis@gmail.com>
2018-06-10 23:56:21 -0300

    Troca extensões scss por css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './Header.scss';
+import './Header.css';
 
 import logoUrl from '../../img/logo-votasp.png';
 
 const Header = () => {
   return (
     <header className="header">
-      <img src={logoUrl} alt="Logotipo escrito Vota em branco e SP em amarelo." />
+      <img
+        src={logoUrl}
+        alt="Logotipo escrito Vota em branco e SP em amarelo."
+      />
     </header>
   );
 };
