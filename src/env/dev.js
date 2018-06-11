0a97fe6a0e767c83899f529352cb48f854681083
Victor Miguez <victorcineis@gmail.com>
2018-06-10 21:29:31 -0300

    Atualiza configurações do auth0

--- src/env/dev.js
+++ src/env/dev.js
@@ -2,6 +2,9 @@ export default {
   auth0: {
     domain: 'minhacampinas.auth0.com',
     clientId: '0511QuKo8LMyLODIwp2mcKbFskxruEsx',
-    callbackUrl: 'http://localhost:1234/login/callback'
+    callbackUrl: 'http://localhost:1234/login/callback',
+    audience: 'https://minhacampinas.auth0.com/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid'
   }
 };
--- src/env/prod.js
+++ src/env/prod.js
@@ -2,6 +2,9 @@ export default {
   auth0: {
     domain: 'login.votasp.org.br',
     clientId: '0511QuKo8LMyLODIwp2mcKbFskxruEsx',
-    callbackUrl: 'http://app.votasp.org.br/login/callback'
+    callbackUrl: 'http://app.votasp.org.br/login/callback',
+    audience: 'https://login.votasp.org.br/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid'
   }
 };
