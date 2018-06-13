18c560c6d5f4e51ace43ec9bae749dcdd4606873
Victor Miguez <victorcineis@gmail.com>
2018-06-12 23:46:11 -0300

    Define domínio customizado para login

--- src/env/prod.js
+++ src/env/prod.js
@@ -1,7 +1,7 @@
 export default {
   auth0: {
     originalDomain: 'minhacampinas.auth0.com',
-    domain: 'minhacampinas.auth0.com',
+    domain: 'login.votasp.org.br',
     clientId: 'xlqRIQx9UxwMkj9XOONKbEn6ksrEt3Pg',
     callbackUrl: 'https://app.votasp.org.br/login/callback',
     responseType: 'token id_token',
