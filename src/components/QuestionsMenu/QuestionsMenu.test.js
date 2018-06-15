e59f060e0e50fdabe645bd13c065e7fc5f09eb5a
degrecci <hdegrecci@gmail.com>
2018-06-14 21:48:09 -0300

    Atualiza e Adiciona teste para menu aberto

--- src/components/QuestionsMenu/QuestionsMenu.test.js
+++ src/components/QuestionsMenu/QuestionsMenu.test.js
@@ -3,19 +3,41 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 import QuestionMenu from './QuestionsMenu';
+import { answersMock, questionsMock } from './QuestionsMenuMock';
 
 describe('QuestionsMenu', () => {
   it('should match a snapshot', () => {
     const questionMenu = shallow((
-      <QuestionMenu />
+      <QuestionMenu
+        answersArray={answersMock}
+        questionsArray={questionsMock}
+        currentQuestion={30}
+      />
     ))
 
     expect(toJson(questionMenu)).toMatchSnapshot();
   })
 
+  it('should match a snapshot when menu is open', () => {
+    const questionMenu = shallow((
+      <QuestionMenu
+        answersArray={answersMock}
+        questionsArray={questionsMock}
+        currentQuestion={30}
+      />
+    ))
+
+    questionMenu.find('.questions-button').simulate('click');
+    expect(toJson(questionMenu)).toMatchSnapshot();
+  })
+
   it('should toggle questions board when click on questions button', () => {
     const questionMenu = shallow((
-      <QuestionMenu />
+      <QuestionMenu
+        answersArray={answersMock}
+        questionsArray={questionsMock}
+        currentQuestion={30}
+      />
     ))
 
     questionMenu.find('.questions-button').simulate('click');
