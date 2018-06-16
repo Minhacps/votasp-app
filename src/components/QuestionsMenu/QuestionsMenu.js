28b51e778b041ffd8c0794fb6c2d1a4634b69e7e
Guilherme Henrique Luchesi <gui.olhenrique@gmail.com>
2018-06-16 15:53:14 -0300

    Troca seta de feita por css para um svg

--- src/components/QuestionsMenu/QuestionsMenu.js
+++ src/components/QuestionsMenu/QuestionsMenu.js
@@ -54,10 +54,9 @@ class QuestionsMenu extends Component {
           onClick={this.toggleQuestionsBoard}
         >
           Questões
-          <div className={classnames(
-            { 'arrow-up': this.state.isOpen },
-            { 'arrow-down': !this.state.isOpen },
-          )}/>
+          <svg className="arrow" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
+            <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/>
+          </svg>
         </button>
 
         {this.state.isOpen &&
