7c3d264433f2ff0f8a5885228173bff0d73ca530
Victor Miguez <victorcineis@gmail.com>
2018-06-16 12:49:56 -0300

    Atribui valores mockados para QuestionsMenu

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import WelcomeMessage from '../components/WelcomeMessage/WelcomeMessage';
 import QuestionsMenu from '../components/QuestionsMenu/QuestionsMenu';
+import {
+  answersMock,
+  questionsMock
+} from '../components/QuestionsMenu/QuestionsMenuMock';
 
 const Home = () => (
   <React.Fragment>
-    <QuestionsMenu />
+    <QuestionsMenu answersArray={answersMock} questionsArray={questionsMock} />
     <WelcomeMessage />
   </React.Fragment>
 );
