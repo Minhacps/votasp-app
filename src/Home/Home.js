8d0666b1fbdc6de30809119938af44293e7a369b
Victor Miguez <victorcineis@gmail.com>
2018-06-16 18:12:34 -0300

    Exibe nome do usuário na página de boas-vindas

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,21 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+
+import Loader from '../components/Loader/Loader';
 import WelcomeMessage from '../components/WelcomeMessage/WelcomeMessage';
 
-const Home = () => <WelcomeMessage />;
+class Home extends PureComponent {
+  render() {
+    if (this.props.auth0.isLoading) {
+      return <Loader />;
+    }
 
-export default Home;
+    return <WelcomeMessage userName={this.props.auth0.userData.name} />;
+  }
+}
+
+const mapStateToProps = state => ({
+  auth0: state.auth0
+});
+
+export default connect(mapStateToProps)(Home);
