fd3410ae3c4be6bfa75f1ee7e6b6e3e7bb565c86
Victor Miguez <victorcineis@gmail.com>
2018-06-30 11:07:06 -0300

    Aplica PageLayout nas páginas existentes

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import PageLayout from '../components/PageLayout/PageLayout';
 import WelcomeMessage from '../components/WelcomeMessage/WelcomeMessage';
 
-const Home = () => <WelcomeMessage />;
+const Home = () => (
+  <PageLayout>
+    <WelcomeMessage />
+  </PageLayout>
+);
 
 export default Home;
