5396eacdd5b35171f7ef602c5f6b0c876c12d9d0
Victor Miguez <victorcineis@gmail.com>
2018-06-30 22:49:51 -0300

    Inicia configuração da management API

--- src/Auth0/Auth0.js
+++ src/Auth0/Auth0.js
@@ -7,11 +7,6 @@ import { storeUserData } from '../redux/modules/auth0';
 
 export default class Auth {
   constructor() {
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
-    this.handleAuthentication = this.handleAuthentication.bind(this);
-    this.isAuthenticated = this.isAuthenticated.bind(this);
-
     this.auth0 = new auth0.WebAuth({
       domain: env.auth0.domain,
       clientID: env.auth0.clientId,
@@ -22,25 +17,26 @@ export default class Auth {
     });
   }
 
-  login() {
+  login = () => {
     this.auth0.authorize();
-  }
+  };
 
-  handleAuthentication() {
-    this.auth0.parseHash((err, authResult) => {
+  handleAuthentication = () => {
+    this.auth0.parseHash((error, authResult) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
         history.replace('/');
-      } else if (err) {
-        console.log(err);
-        window.alert(
-          `Ocorreu um erro no login, cheque o console para mais detalhes.`
-        );
       }
     });
-  }
+  };
 
-  setSession(authResult) {
+  setSession = authResult => {
+    console.log(authResult);
     let expiresAt = JSON.stringify(
       authResult.expiresIn * 1000 + new Date().getTime()
     );
@@ -52,45 +48,41 @@ export default class Auth {
     this.getUserInfo();
 
     history.replace('/');
-  }
+  };
 
-  logout() {
+  logout = () => {
     window.localStorage.removeItem('access_token');
     window.localStorage.removeItem('id_token');
     window.localStorage.removeItem('expires_at');
     history.replace('/');
-  }
+  };
 
-  isAuthenticated() {
+  isAuthenticated = () => {
     let expiresAt = JSON.parse(window.localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
-  }
+  };
 
-  getUserInfo() {
+  getUserInfo = () => {
+    const idToken = window.localStorage.getItem('id_token');
     const accessToken = window.localStorage.getItem('access_token');
 
     if (!accessToken) {
       return;
     }
 
-    console.log(this.auth0);
-    this.auth0.checkSession(
-      {
-        audience: `https://minhacampinas.auth0.com/api/v2/`,
-        scope: 'read:current_user'
-      },
-      (err, result) => {
-        console.log(err);
-        console.log(result);
-      }
-    );
+    const auth0Manage = new auth0.Management({
+      domain: env.auth0.domain,
+      token: idToken
+    });
 
     this.auth0.client.userInfo(accessToken, (error, userData) => {
       if (error) {
         throw error;
       }
 
+      auth0Manage.getUser(userData.sub, console.log);
+
       store.dispatch(storeUserData(userData));
     });
-  }
+  };
 }
