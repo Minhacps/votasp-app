1ae9ac13f9b0e5d3fe667be641fad93447ea42e5
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-07-05 00:16:36 -0300

    adiciona botoes e acao e seus métodos; usa service getQuestions

--- src/Questionario/Questionario.js
+++ src/Questionario/Questionario.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 
 import store from '../redux/store';
 import Pergunta from '../components/Pergunta/Pergunta';
 import { storePerguntas } from '../redux/modules/perguntas';
+import { getQuestions } from '../services/questions';
 
 import './Questionario.css';
 
 export class RawQuestionario extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentQuestion: 0,
+    };
   };
 
   componentDidMount() {
-    axios.get('https://questions.api.votasp.org.br/')
+    getQuestions()
       .then(response => {
         const perguntas = response.data;
         store.dispatch(storePerguntas(perguntas));
@@ -22,12 +26,37 @@ export class RawQuestionario extends Component {
       .catch(error => console.log(error));
   }
 
+  pularQuestao = () => {
+    const { currentQuestion } = this.state;
+    const { perguntas } = this.props;
+
+    if (currentQuestion === perguntas.length - 1) {
+      return;
+    }
+
+    this.setState({
+      currentQuestion: currentQuestion + 1,
+    });
+
+    // TODO: Chamar api respondendo como INDIFERENTE
+  }
+
+  goToRanking = () => {
+    // TODO: Navegar para pagina de ranking
+  }
+
   render() {
+    const { currentQuestion } = this.state;
     const { perguntas } = this.props;
 
     return (
       <div className="questionario__container">
-        {perguntas.length && <Pergunta pergunta={perguntas[0]} />}
+        {perguntas.length && <Pergunta pergunta={perguntas[currentQuestion]} />}
+
+        <div className="questionario__actions-container">
+          <button onClick={this.pularQuestao} className="btn btn-light">Pular</button>
+          <button onClick={this.goToRanking} disabled className="btn btn-light">Calcular afinidade</button>
+        </div>
       </div>
     );
   }
