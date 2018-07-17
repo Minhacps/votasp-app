26cf3ee97cc61cb8c21798191a6d11a2cc223cc7
Victor Miguez <victorcineis@gmail.com>
2018-07-17 00:22:29 -0300

    Remove loader da home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 
-import Loader from '../components/Loader/Loader';
 import PageLayout from '../components/PageLayout/PageLayout';
 import WelcomeMessage from '../components/WelcomeMessage/WelcomeMessage';
 import QuestionsMenu from '../components/QuestionsMenu/QuestionsMenu';
@@ -9,10 +8,6 @@ import { answersMock, questionsMock } from '../components/QuestionsMenu/Question
 
 class Home extends PureComponent {
   render() {
-    if (this.props.auth0.isLoading) {
-      return <Loader />;
-    }
-
     return (
       <PageLayout>
         <WelcomeMessage userName={this.props.auth0.userData.name} />
