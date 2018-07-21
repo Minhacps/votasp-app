d7c90c8871d693c256939d01568a6d20504a26df
Live session user <hdegrecci@gmail.com>
2018-07-21 17:39:48 +0000

    Remove QuestionsMenu de Home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,15 +3,12 @@ import { connect } from 'react-redux';
 
 import PageLayout from '../components/PageLayout/PageLayout';
 import WelcomeMessage from '../components/WelcomeMessage/WelcomeMessage';
-import QuestionsMenu from '../components/QuestionsMenu/QuestionsMenu';
-import { answersMock, questionsMock } from '../components/QuestionsMenu/QuestionsMenuMock';
 
 class Home extends PureComponent {
   render() {
     return (
       <PageLayout>
         <WelcomeMessage userName={this.props.auth0.userData.name} />
-        <QuestionsMenu answersArray={answersMock} questionsArray={questionsMock} />
       </PageLayout>
     );
   }
