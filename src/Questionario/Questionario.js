f8e077621609e1108eb522e60f4a6b22f2afedb4
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-07-21 13:22:59 -0300

    implementa comunicacao com o firebase

--- src/Questionario/Questionario.js
+++ src/Questionario/Questionario.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import classnames from 'classnames';
 
 import store from '../redux/store';
 import Pergunta from '../components/Pergunta/Pergunta';
 import PageLayout from '../components/PageLayout/PageLayout';
 import { storePerguntas } from '../redux/modules/perguntas';
+import { INDIFERENTE } from '../constants/respostas';
 import questoes from './questoes'
+import firebase from 'firebase/app';
 
 import './Questionario.css';
 
 export class RawQuestionario extends Component {
 
   state = {
-    currentQuestion: 0
+    currentQuestion: 0,
+    isAnswering: false,
   };
 
   componentDidMount() {
@@ -20,6 +24,41 @@ export class RawQuestionario extends Component {
   }
 
   pularQuestao = () => {
+    this.saveAnswer(INDIFERENTE).then(() => {
+      this.proximaQuestao();
+    });
+  };
+
+  goToRanking = () => {
+    // TODO: Navegar para pagina de ranking
+  };
+
+  responderQuestao = (event) => {
+    this.saveAnswer(event.target.value).then(() => {
+      this.proximaQuestao();
+    });
+  };
+
+  saveAnswer = (answerValue) => {
+    const userId = firebase.auth().currentUser.uid;
+    const { currentQuestion } = this.state;
+    const questionId = currentQuestion + 1;
+    const answer = {
+      [questionId]: answerValue,
+    };
+
+    this.setState({
+      isAnswering: true,
+    });
+
+    return firebase
+      .firestore()
+      .collection('answers')
+      .doc(userId)
+      .set(answer, { merge: true });
+  }
+
+  proximaQuestao = () => {
     const { currentQuestion } = this.state;
     const { perguntas } = this.props;
 
@@ -28,30 +67,42 @@ export class RawQuestionario extends Component {
     }
 
     this.setState({
-      currentQuestion: currentQuestion + 1
+      currentQuestion: currentQuestion + 1,
+      isAnswering: false,
     });
-
-    // TODO: Chamar api respondendo como INDIFERENTE
-  };
-
-  goToRanking = () => {
-    // TODO: Navegar para pagina de ranking
-  };
+  }
 
   render() {
-    const { currentQuestion } = this.state;
+    const { currentQuestion, isAnswering } = this.state;
     const { perguntas } = this.props;
 
     return (
       <PageLayout>
-        <div className="questionario__container">
-          {perguntas.length && <Pergunta pergunta={perguntas[currentQuestion]} />}
+        <div className={classnames(
+          'questionario__container',
+          { 'questionario__container--loading': isAnswering }
+        )}>
+          {perguntas.length &&
+            <Pergunta
+              pergunta={perguntas[currentQuestion]}
+              responderQuestao={this.responderQuestao}
+              isAnswering={isAnswering}
+            />
+          }
 
           <div className="questionario__actions-container">
-            <button onClick={this.pularQuestao} className="btn btn-light">
+            <button
+              onClick={this.pularQuestao}
+              className="btn btn-light"
+              disabled={isAnswering}
+            >
               Pular
             </button>
-            <button onClick={this.goToRanking} disabled className="btn btn-light">
+            <button
+              onClick={this.goToRanking}
+              className="btn btn-light"
+              disabled={isAnswering}
+            >
               Calcular afinidade
             </button>
           </div>
