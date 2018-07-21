aa7b8fa20e29404e6538cff64df4707ebf40b456
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2018-07-21 15:16:38 -0300

    enable offline

--- src/firebaseSetup.js
+++ src/firebaseSetup.js
@@ -5,16 +5,17 @@ import 'firebase/firestore';
 import currentEnv from './env';
 
 const firebaseSetup = () => {
-  const config = {
-    apiKey: currentEnv.firebase.apiKey,
-    authDomain: currentEnv.firebase.authDomain,
-    projectId: currentEnv.firebase.projectId
-  };
+    const config = {
+        apiKey: currentEnv.firebase.apiKey,
+        authDomain: currentEnv.firebase.authDomain,
+        projectId: currentEnv.firebase.projectId
+    };
 
-  if (!firebase.apps.length) {
-    firebase.initializeApp(config);
-    firebase.firestore().settings({ timestampsInSnapshots: true });
-  }
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+        firebase.firestore().settings({ timestampsInSnapshots: true });
+        firebase.firestore().enablePersistence();
+    }
 };
 
 export default firebaseSetup;
