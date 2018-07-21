b79554da60ea95405cee149dcd45c062c4efe4e6
Victor Miguez <victorcineis@gmail.com>
2018-07-21 10:21:30 -0300

    Finaliza persistência de dados cadastrais

--- src/components/CompleteSignup/CompleteSignup.js
+++ src/components/CompleteSignup/CompleteSignup.js
@@ -1,21 +1,43 @@
 import React, { PureComponent } from 'react';
+import firebase from 'firebase/app';
 
 import PageLayout from '../PageLayout/PageLayout';
 import CompleteSignupForm from './CompleteSignupForm';
+
+import { VOTER, CANDIDATE } from '../../constants/userRoles';
 
 class CompleteSignup extends PureComponent {
   handleSubmit = event => {
     event.preventDefault();
 
+    const userData = this.formatUserData(event.target);
+    const userId = firebase.auth().currentUser.uid;
+    firebase
+      .firestore()
+      .collection('users')
+      .doc(userId)
+      .update(userData);
+  };
+
+  formatUserData = fields => {
     const userMetadata = {
-      userProfile: this.state.userProfile
+      role: fields.role.value
     };
 
+    if (userMetadata.role === VOTER) {
+      return userMetadata;
+    }
+
     const candidateData = {
-      candidateType: event.target.candidateType.value,
-      candidateCnpj: event.target.candidateCnpj.value,
-      candidateNumber: event.target.candidateNumber.value,
-      politicalParty: event.target.politicalParty.value
+      level: fields.level.value,
+      cnpj: fields.cnpj.value,
+      number: fields.number.value,
+      politicalParty: fields.politicalParty.value
+    };
+
+    return {
+      ...userMetadata,
+      ...candidateData
     };
   };
 
