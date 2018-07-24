a02353590649351b53ecd4f4c7dc84b7d8757c32
Victor Miguez <victorcineis@gmail.com>
2018-07-24 08:41:27 -0300

    Atualiza configurações com firebase de produção

--- src/env/prod.js
+++ src/env/prod.js
@@ -8,8 +8,8 @@ export default {
     scope: 'openid'
   },
   firebase: {
-    apiKey: 'AIzaSyBVclKJQrkvdVhj0gL4E5idZ1t0BBhbaXs',
-    authDomain: 'vota-sp-dev.firebaseapp.com',
-    projectId: 'vota-sp-dev'
+    apiKey: 'AIzaSyCSJrmRabzA9DMcvWipsvnh_e2OlwFuh1s',
+    authDomain: 'vota-sp-prod.firebaseapp.com',
+    projectId: 'vota-sp-prod'
   }
 };
