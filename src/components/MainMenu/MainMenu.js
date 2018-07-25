126aa75adf8f1f09ba5fa2721beeef93715d9de4
Guilherme Henrique Luchesi <gui.olhenrique@gmail.com>
2018-07-25 07:24:42 -0300

    Substitue svg generico por componente de hamburger menu

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -4,6 +4,12 @@ import classnames from 'classnames';
 
 import './MainMenu.css';
 
+const HamburgerMenu = ({ onClick, title }) => (
+  <button className="navigation-menu__link navigation-menu__toggle" type="button" onClick={onClick} >
+    <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 22.67 22.82"><title>Hamburguer Menu</title><g><path class="listraHamburguer" d="M20.94,0H1.7A1.71,1.71,0,0,0,0,1.7H0A1.72,1.72,0,0,0,1.7,3.4H21a1.72,1.72,0,0,0,1.7-1.7h0A1.74,1.74,0,0,0,20.94,0Z"/><path class="listraHamburguer" d="M20.94,19.42H1.7A1.72,1.72,0,0,0,0,21.12H0a1.71,1.71,0,0,0,1.7,1.7H21a1.72,1.72,0,0,0,1.7-1.7h0A1.75,1.75,0,0,0,20.94,19.42Z"/><path class="listraHamburguer" d="M20.94,9.73H1.7A1.71,1.71,0,0,0,0,11.43H0a1.71,1.71,0,0,0,1.7,1.69H21a1.72,1.72,0,0,0,1.7-1.69h0A1.74,1.74,0,0,0,20.94,9.73Z"/></g></svg>
+  </button>
+)
+
 class MainMenu extends Component {
   constructor(props) {
     super(props);
@@ -26,13 +32,9 @@ class MainMenu extends Component {
         'navigation-menu',
         { 'opened': this.state.isOpen },
       )}>
-        <button className="navigation-menu__link navigation-menu__toggle" type="button" onClick={this.toggleMainMenu} >
-          <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 22.67 22.82"><title>Menu Mobile</title><g><path class="listraHamburguer" d="M20.94,0H1.7A1.71,1.71,0,0,0,0,1.7H0A1.72,1.72,0,0,0,1.7,3.4H21a1.72,1.72,0,0,0,1.7-1.7h0A1.74,1.74,0,0,0,20.94,0Z"/><path class="listraHamburguer" d="M20.94,19.42H1.7A1.72,1.72,0,0,0,0,21.12H0a1.71,1.71,0,0,0,1.7,1.7H21a1.72,1.72,0,0,0,1.7-1.7h0A1.75,1.75,0,0,0,20.94,19.42Z"/><path class="listraHamburguer" d="M20.94,9.73H1.7A1.71,1.71,0,0,0,0,11.43H0a1.71,1.71,0,0,0,1.7,1.69H21a1.72,1.72,0,0,0,1.7-1.69h0A1.74,1.74,0,0,0,20.94,9.73Z"/></g></svg>
-        </button>
+        <HamburgerMenu onClick={this.toggleMainMenu} />
         <ul className="navigation-menu">
-          <button className="navigation-menu__link navigation-menu__toggle" type="button" onClick={this.toggleMainMenu} >
-            <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 22.67 22.82"><title>Menu Mobile</title><g><path class="listraHamburguer" d="M20.94,0H1.7A1.71,1.71,0,0,0,0,1.7H0A1.72,1.72,0,0,0,1.7,3.4H21a1.72,1.72,0,0,0,1.7-1.7h0A1.74,1.74,0,0,0,20.94,0Z"/><path class="listraHamburguer" d="M20.94,19.42H1.7A1.72,1.72,0,0,0,0,21.12H0a1.71,1.71,0,0,0,1.7,1.7H21a1.72,1.72,0,0,0,1.7-1.7h0A1.75,1.75,0,0,0,20.94,19.42Z"/><path class="listraHamburguer" d="M20.94,9.73H1.7A1.71,1.71,0,0,0,0,11.43H0a1.71,1.71,0,0,0,1.7,1.69H21a1.72,1.72,0,0,0,1.7-1.69h0A1.74,1.74,0,0,0,20.94,9.73Z"/></g></svg>
-          </button>
+          <HamburgerMenu onClick={this.toggleMainMenu} />
           <li className="navigation-menu__list">
             <NavLink to="/meu-perfil" activeClassName="active" className="navigation-menu__link">
               Meu perfil
