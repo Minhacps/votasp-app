528424c4917c243d54e705e2ca779a642c21a026
Guilherme Henrique Luchesi <gui.olhenrique@gmail.com>
2018-07-26 20:41:27 -0300

    Corrige class para className no hamburger menu

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -6,9 +6,9 @@ import classnames from 'classnames';
 
 import './MainMenu.css';
 
-const HamburgerMenu = ({ onClick, title }) => (
-  <button className="navigation-menu__link navigation-menu__toggle" type="button" onClick={onClick} >
-    <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 22.67 22.82"><title>Hamburguer Menu</title><g><path class="listraHamburguer" d="M20.94,0H1.7A1.71,1.71,0,0,0,0,1.7H0A1.72,1.72,0,0,0,1.7,3.4H21a1.72,1.72,0,0,0,1.7-1.7h0A1.74,1.74,0,0,0,20.94,0Z"/><path class="listraHamburguer" d="M20.94,19.42H1.7A1.72,1.72,0,0,0,0,21.12H0a1.71,1.71,0,0,0,1.7,1.7H21a1.72,1.72,0,0,0,1.7-1.7h0A1.75,1.75,0,0,0,20.94,19.42Z"/><path class="listraHamburguer" d="M20.94,9.73H1.7A1.71,1.71,0,0,0,0,11.43H0a1.71,1.71,0,0,0,1.7,1.69H21a1.72,1.72,0,0,0,1.7-1.69h0A1.74,1.74,0,0,0,20.94,9.73Z"/></g></svg>
+const HamburgerMenu = ({ onClick }) => (
+  <button className="hamburger-menu" type="button" onClick={onClick} >
+    <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 22.67 22.82"><title>Hamburguer Menu</title><g><path className="listraHamburguer" d="M20.94,0H1.7A1.71,1.71,0,0,0,0,1.7H0A1.72,1.72,0,0,0,1.7,3.4H21a1.72,1.72,0,0,0,1.7-1.7h0A1.74,1.74,0,0,0,20.94,0Z"/><path className="listraHamburguer" d="M20.94,19.42H1.7A1.72,1.72,0,0,0,0,21.12H0a1.71,1.71,0,0,0,1.7,1.7H21a1.72,1.72,0,0,0,1.7-1.7h0A1.75,1.75,0,0,0,20.94,19.42Z"/><path className="listraHamburguer" d="M20.94,9.73H1.7A1.71,1.71,0,0,0,0,11.43H0a1.71,1.71,0,0,0,1.7,1.69H21a1.72,1.72,0,0,0,1.7-1.69h0A1.74,1.74,0,0,0,20.94,9.73Z"/></g></svg>
   </button>
 )
 
