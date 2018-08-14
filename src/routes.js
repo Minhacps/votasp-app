e68559dd565bcf6e6f8e90c9df914fed949c0ae0
Victor Miguez <victorcineis@gmail.com>
2018-08-14 13:03:26 -0300

    Define prefixo /app para rotas privadas

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import LandingPage from './pages/LandingPage/LandingPage';
 import Home from './Home/Home';
 import Results from './Results/Results';
 import ResultsWarning from './ResultsWarning/ResultsWarning';
@@ -9,11 +10,12 @@ import ComoFunciona from './components/ComoFunciona/ComoFunciona';
 
 const Routes = () => (
   <React.Fragment>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={LandingPage} />
     <Route exact path="/como-funciona" component={ComoFunciona} />
-    <Route path="/questionario/:question" component={Questionario} />
-    <Route exact path="/calculando-ranking" component={ResultsWarning} />
-    <Route exact path="/ranking" component={Results} />
+    <Route exact path="/app" component={Home} />
+    <Route path="/app/questionario/:question" component={Questionario} />
+    <Route exact path="/app/calculando-ranking" component={ResultsWarning} />
+    <Route exact path="/app/ranking" component={Results} />
   </React.Fragment>
 );
 
