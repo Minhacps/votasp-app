1980311dc5afe8129b6f5a66e54fb088784e3045
Victor Miguez <victorcineis@gmail.com>
2018-08-16 10:18:17 -0300

    Exibe passos de como funciona na landing page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Header from '../../components/Header/Header';
+import PassosComoFunciona from '../../components/ComoFunciona/PassosComoFunciona';
+
 const LandingPage = () => (
-  <div>LandingPage</div>
+  <React.Fragment>
+    <Header />
+    <div className="como-funciona">
+      <PassosComoFunciona />
+    </div>
+  </React.Fragment>
 );
 
 export default LandingPage;
