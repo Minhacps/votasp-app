501122188ff63629929e27d867ca912d4879cebf
André Luís Bordignon <albordignon@gmail.com>
2018-08-25 15:34:14 -0300

    Inicio do desenvolvimento da página de realização

---
+++ src/components/Realizacao/Realizacao.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './Realizacao.scss';
+
+import Amanda from '../../img/realizacao/Amanda.png';
+import Anderson from '../../img/realizacao/Anderson.png';
+import Andre from '../../img/realizacao/Andre.png';
+import Bruno from '../../img/realizacao/Bruno.png';
+import Claudia from '../../img/realizacao/Claudia.png';
+import Eduardo from '../../img/realizacao/Eduardo.png';
+import Elisa from '../../img/realizacao/Elisa.png';
+import Gabriel from '../../img/realizacao/GRibeiro.png';
+import Giulia from '../../img/realizacao/Giulia.png';
+import Guilherme from '../../img/realizacao/Guilherme.png';
+import Henrique from '../../img/realizacao/Henrique.png';
+import Juliana from '../../img/realizacao/Juliana.png';
+import Lai from '../../img/realizacao/Lai.png';
+import Lucas from '../../img/realizacao/Lucas.png';
+import Marcelo from '../../img/realizacao/Marcelo.png';
+import Mathias from '../../img/realizacao/Mathias.png';
+import Van from '../../img/realizacao/Van.png';
+import VMiguez from '../../img/realizacao/VMiguez.png';
+import VPerin from '../../img/realizacao/VPerin.png';
+
+const realizacao = () => {
+  return (
+    <section className='Realizacao'>
+      <h2>Realizacão</h2>
+      <p>O Vota SP foi promovido pela Minha Campinas e desenvolvido em parceria com o Instituto Federal de São Paulo - Campinas e com o FrontEnd Campinas, um grupo de programadores e iprogramadoras independentes que utilizam tecnologia para a construção de um sociedade melhor.
+    </p>
+      <section className='Photos'>
+        <img src={Amanda} alt='Foto da Amanda' />
+        <img src={Anderson} alt='Foto do Anderson' />
+        <img src={Andre} alt='Foto do Andre' />
+        <img src={Bruno} alt='Foto do Bruno' />
+        <img src={Claudia} alt='Foto do Claudia' />
+        <img src={Eduardo} alt='Foto do Eduardo' />
+        <img src={Elisa} alt='Foto do Elsia' />
+        <img src={Gabriel} alt='Foto do Gabriel' />
+        <img src={Giulia} alt='Foto do Giulia' />
+        <img src={Guilherme} alt='Foto do Guilherme' />
+        <img src={Henrique} alt='Foto do Henrique' />
+        <img src={Juliana} alt='Foto do Juliana' />
+        <img src={Lai} alt='Foto do Lai' />
+        <img src={Lucas} alt='Foto do Lucas' />
+        <img src={Marcelo} alt='Foto do Marcelo' />
+        <img src={Mathias} alt='Foto do Mathias' />
+        <img src={Van} alt='Foto do Vanessa' />
+        <img src={VMiguez} alt='Foto do Victor Miguez' />
+        <img src={VPerin} alt='Foto do Vitor Perin' />
+      </section>
+
+    </section>
+  );
+}
+
+export default realizacao;
