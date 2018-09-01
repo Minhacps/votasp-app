7a849c236cf69a813e2c8c7465e967209928b62c
Victor Miguez <victorcineis@gmail.com>
2018-09-01 11:31:24 -0300

    Remove código obsoleto de autenticação

--- src/env/prod.js
+++ src/env/prod.js
@@ -1,12 +1,4 @@
 export default {
-  auth0: {
-    originalDomain: 'minhacampinas.auth0.com',
-    domain: 'login.votasp.org.br',
-    clientId: 'xlqRIQx9UxwMkj9XOONKbEn6ksrEt3Pg',
-    callbackUrl: 'https://app.votasp.org.br/login/callback',
-    responseType: 'token id_token',
-    scope: 'openid'
-  },
   firebase: {
     apiKey: 'AIzaSyCSJrmRabzA9DMcvWipsvnh_e2OlwFuh1s',
     authDomain: 'vota-sp-prod.firebaseapp.com',
