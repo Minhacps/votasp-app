1769d0ec99589785b6faffc1cc865fba065ca3ab
Mathias54 <4n0ny2015>
2018-09-02 02:30:17 -0300

    Correção de dependência e adição do ambiente de produção.

--- src/env/prod.js
+++ src/env/prod.js
@@ -8,8 +8,11 @@ export default {
     scope: 'openid'
   },
   firebase: {
-    apiKey: 'AIzaSyCSJrmRabzA9DMcvWipsvnh_e2OlwFuh1s',
-    authDomain: 'vota-sp-prod.firebaseapp.com',
-    projectId: 'vota-sp-prod'
+    apiKey: "AIzaSyAbcUCnf-5XhNaliTNTm6UuB0F66iKhl6o",
+    authDomain: "meu-voto-prd.firebaseapp.com",
+    databaseURL: "https://meu-voto-prd.firebaseio.com",
+    projectId: "meu-voto-prd",
+    storageBucket: "meu-voto-prd.appspot.com",
+    messagingSenderId: "748726645910"
   }
 };
