9e711fe60c69a8c699ee9a8965ada3bf2f58f898
Victor Miguez <victorcineis@gmail.com>
2018-09-06 00:23:46 -0300

    Aplica componente FormLayout para recuperação de senha

--- src/components/RedefinePassword/RedefinePassword.js
+++ src/components/RedefinePassword/RedefinePassword.js
@@ -2,69 +2,87 @@ import React, { Component } from 'react';
 import firebase from 'firebase/app';
 import PageLayout from '../PageLayout/PageLayout';
 
+import FormLayout from '../Authentication/FormLayout';
+
 import './RedefinePassword.scss';
 
 class RedefinePassword extends Component {
+  state = {
+    successMessage: null,
+    errorMessage: null
+  };
 
-	state = {
-		errorMessage: null
-	}
+  onSubmit = event => {
+    event.preventDefault();
+    const emailAddress = event.target.email.value;
+    var auth = firebase.auth();
+    auth
+      .sendPasswordResetEmail(emailAddress)
+      .then(doc => {
+        this.handleSendEmailSuccess();
+      })
+      .catch(this.handleSendEmailFailure);
+  };
 
-	onSubmit = event => {
-		event.preventDefault();
-		const emailAddress = event.target.email.value;
-		var auth = firebase.auth();
-		auth.sendPasswordResetEmail(emailAddress).then(doc => {
-			this.handleSendEmailSuccess();
-		}).catch(this.handleSendEmailFailure);
-	}
+  handleSendEmailSuccess = () => {
+    return this.updateSuccessMessage('Email enviado com sucesso.');
+  };
 
-	handleSendEmailSuccess = () => {
-		return this.updateErrorMessage('Email enviado com sucesso.');
-	}
+  handleSendEmailFailure = error => {
+    console.log(error);
+    switch (error.code) {
+      case 'auth/invalid-email': {
+        return this.updateErrorMessage('O formato do e-mail informado é inválido.');
+      }
+      case 'auth/user-disabled': {
+        return this.updateErrorMessage('Usuário desabilitado.');
+      }
 
-	handleSendEmailFailure = error => {
-		console.log(error);
-		switch (error.code) {
-		  case 'auth/invalid-email': {
-			return this.updateErrorMessage('O formato do e-mail informado é inválido.');
-		  }
-		  case 'auth/user-disabled': {
-			return this.updateErrorMessage('Usuário desabilitado.');
-		  }
-	
-		  case 'auth/user-not-found':{
-			return this.updateErrorMessage('E-mail não encontrado na base de dados.');
-		  }
-	
-		  default: {
-			return this.updateErrorMessage('Ocorreu um erro inesperado.');
-		  }
-		}
-	  };
+      case 'auth/user-not-found': {
+        return this.updateErrorMessage('E-mail não encontrado na base de dados.');
+      }
 
-	updateErrorMessage = errorMessage => {
-		this.setState({ errorMessage });
-	};
+      default: {
+        return this.updateErrorMessage('Ocorreu um erro inesperado.');
+      }
+    }
+  };
 
-	render() {
-		return (
-			<PageLayout>
-				<section className='RedefinePassword'>
-					<form onSubmit={this.onSubmit}>
-						<div className="field-wrapper">
-							<label htmlFor="email">E-mail</label>
-							<input type="text" className="input" name="email" id="email" required placeholder="Digite o e-mail para recuperar a senha" />
-						</div>
-						<button className="SubmitButton">Enviar</button>
-					</form>
-					<div>
-						{this.state.errorMessage}
-					</div>
-				</section>
-			</PageLayout>
-		)
-	}
+  updateSuccessMessage = successMessage => {
+    this.setState({ successMessage, errorMessage: null });
+  };
+
+  updateErrorMessage = errorMessage => {
+    this.setState({ errorMessage, successMessage: null });
+  };
+
+  render() {
+    return (
+      <FormLayout
+        hideTabs
+        successMessage={this.state.successMessage}
+        errorMessage={this.state.errorMessage}
+      >
+        <h1 className="redefine-password__title">Recuperação de senha</h1>
+        <section className="authentication__form-content">
+          <form onSubmit={this.onSubmit}>
+            <div className="field-wrapper">
+              <label htmlFor="email">E-mail</label>
+              <input
+                type="text"
+                className="input"
+                name="email"
+                id="email"
+                required
+                placeholder="Informe o e-mail cadastrado"
+              />
+            </div>
+          </form>
+        </section>
+        <button className="authentication__submit-button">Recuperar senha</button>
+      </FormLayout>
+    );
+  }
 }
 
 export default RedefinePassword;
